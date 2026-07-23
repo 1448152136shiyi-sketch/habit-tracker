e2f1e83212722b7fcb64d3d90032137131fdb9d4
1448152136shiyi-sketch <1448152136shiyi@gmail.com>
2026-07-23 17:20:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,28 @@
-const CACHE = 'ht-v2';
-const FILES = ['index.html', 'manifest.json'];
+const CACHE = 'ht-v3';
 
-// 每次都先联网，连不上才用缓存
+// 立即接管，不等旧版本关闭
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+// 激活时干掉所有旧缓存
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// 联网优先
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request)
       .then(res => {
-        // 拿到新版就更新缓存
         const clone = res.clone();
         caches.open(CACHE).then(c => c.put(e.request, clone));
         return res;
       })
-      .catch(() => caches.match(e.request)) // 离线用缓存
-  );
-});
-
-// 激活时清旧缓存
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+      .catch(() => caches.match(e.request))
   );
 });
