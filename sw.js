31769367c278e349666d02bd2d31a4d7f36c9308
1448152136shiyi-sketch <1448152136shiyi@gmail.com>
2026-07-23 17:14:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
-const CACHE = 'ht-v1';
+const CACHE = 'ht-v2';
 const FILES = ['index.html', 'manifest.json'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-});
-
+// 每次都先联网，连不上才用缓存
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request)
+      .then(res => {
+        // 拿到新版就更新缓存
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request)) // 离线用缓存
+  );
+});
+
+// 激活时清旧缓存
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
 });
